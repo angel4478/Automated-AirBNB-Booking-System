76cb9efc9fe452f484c7a2eefd74033b51740638
skyclouds-night <jcmjalquitran@gmail.com>
2026-03-18 22:42:26 +0800

    starting stuff lang, will fix further

---
+++ UserAuth.java
@@ -0,0 +1,10 @@
+public class UserAuth {
+
+    public static boolean isUserAuthenticated = false;
+    public static String username = "";
+    public static String password = "";
+
+    public static boolean authenticateUser(String username, String password) {
+        return true;
+    }
+}
